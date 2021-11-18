d4d677947b7491f9d275c361214557ab627ab58a
ammar <ammarraneez@gmail.com>
2021-11-18 18:41:31 +0530

    feat: header details

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
 
+const form = document.querySelector('[data-form]');
 const queryParamsContainer = document.querySelector('[data-query-params]');
 const requestHeadersContainer = document.querySelector('[data-request-headers]');
+const responseHeadersContainer = document.querySelector('[data-response-headers]')
 const keyValueTemplate = document.querySelector('[data-key-value-template]');
+
+queryParamsContainer.append(createKeyValuePair());
+requestHeadersContainer.append(createKeyValuePair());
 
 document.querySelector('[data-add-query-param-btn]').addEventListener('click', (e) => {
   queryParamsContainer.append(createKeyValuePair());
@@ -13,9 +19,40 @@ document.querySelector('[data-add-request-header-btn]').addEventListener('click'
   requestHeadersContainer.append(createKeyValuePair());
 });
 
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  axios({
+    url: document.querySelector('[data-url]').value,
+    method: document.querySelector('[data-method]').value,
+    params: keyValuePairsToObject(queryParamsContainer),
+    headers: keyValuePairsToObject(requestHeadersContainer),
+  }).then((res) => {
+    document.querySelector('[data-response-section').classList.remove('d-none');
+    updateResponseDetails(res);
+    updateResponseEditor(res.data);
+    updateResponseHeaders(res.headers);
+  });
+});
 
-queryParamsContainer.append(createKeyValuePair());
-requestHeadersContainer.append(createKeyValuePair());
+function updateResponseDetails(res) {
+
+}
+
+function updateResponseEditor(res) {
+
+}
+
+function updateResponseHeaders(headers) {
+  responseHeadersContainer.innerHTML = '';
+  Object.entries(headers).forEach(([key, value]) => {
+    const keyElement = document.createElement('div');
+    keyElement.textContent = key;
+    responseHeadersContainer.append(keyElement);
+    const valueElement = document.createElement('div');
+    valueElement.textContent = value;
+    responseHeadersContainer.append(valueElement);
+  });
+}
 
 function createKeyValuePair() {
   const element = keyValueTemplate.content.cloneNode(true);
@@ -24,4 +61,15 @@ function createKeyValuePair() {
   });
 
   return element;
+}
+
+function keyValuePairsToObject(container) {
+  const pairs = container.querySelectorAll('[data-key-value-pair]');
+  return [...pairs].reduce((data, pair) => {
+    const key = pair.querySelector('[data-key]').value;
+    const value = pair.querySelector('[data-value]').value;
+
+    if (key === '') return data;
+    return { ...data, [key]: value };
+  });
 }
